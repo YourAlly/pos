b701e40ec54c90e72a038f167ece74da175bb647
Your Ally <royettemiranda@yahoo.com>
2022-05-31 01:32:20 +0800

    Cleaned up

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once("./init.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Shop</title>
+    <title>PoS</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
